5e97aa5b6ab27bcd0c59c6aa2d8d0d0f8f58baba
gatosyocora <gatosyocora36@gmail.com>
2020-06-19 01:51:23 +0900

    Unity上でEmoteSwitchV3をテストできるプロトタイプを作成

---
+++ Assets/EmoteSwitch V3/EmoteSwitchV3Editor/Editor/EmoteSwitchV3TesterEditor.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EmoteSwitchV3Tester))]
+public class EmoteSwitchV3TesterEditor : Editor
+{
+    private bool startWaiting = false;
+
+    public override void OnInspectorGUI()
+    {
+        var tester = target as EmoteSwitchV3Tester;
+
+        base.OnInspectorGUI();
+        using (new EditorGUI.DisabledGroupScope(!EditorApplication.isPlaying))
+        {
+            if (GUILayout.Button("Reset"))
+            {
+                tester.animator.SetInteger("Emote", 0);
+            }
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("Emote1&2");
+                if (GUILayout.Button("ON"))
+                {
+                    tester.animator.SetInteger("Emote", 1);
+                }
+                if (GUILayout.Button("OFF"))
+                {
+                    tester.animator.SetInteger("Emote", 2);
+                }
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("Emote3&4");
+                if (GUILayout.Button("ON"))
+                {
+                    tester.animator.SetInteger("Emote", 3);
+                }
+                if (GUILayout.Button("OFF"))
+                {
+                    tester.animator.SetInteger("Emote", 4);
+                }
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("Emote5&6");
+                if (GUILayout.Button("ON"))
+                {
+                    tester.animator.SetInteger("Emote", 5);
+                }
+                if (GUILayout.Button("OFF"))
+                {
+                    tester.animator.SetInteger("Emote", 6);
+                }
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("Emote7&8");
+                if (GUILayout.Button("ON"))
+                {
+                    tester.animator.SetInteger("Emote", 7);
+                }
+                if (GUILayout.Button("OFF"))
+                {
+                    tester.animator.SetInteger("Emote", 8);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/EmoteSwitch V3/EmoteSwitchV3Editor/EmoteSwitchV3Tester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRCSDK2;
+
+public class EmoteSwitchV3Tester : MonoBehaviour
+{
+    [NonSerialized]
+    public Animator animator;
+
+    private VRC_AvatarDescriptor avatar;
+
+    public void Start()
+    {
+        avatar = transform.parent.GetComponent<VRC_AvatarDescriptor>();
+        animator = transform.parent.GetComponent<Animator>();
+
+        animator.runtimeAnimatorController = avatar.CustomStandingAnims;
+    }
+}
